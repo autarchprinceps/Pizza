f3c7d5d9c202ed37e0972e631ebed2a40ea12773
autarch princeps <autarch@outlook.com>
2013-12-01 21:24:21 +0100

    Composite pattern design

---
+++ Sinnvolle Implementierung 3.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+enum size { Piccolo, Std, Grande };
+enum belag { cheese, salami }; // ... 
+
+public static class Preis {
+    public static Dictionary<size, int> sizepay = new Dictionary<size,int>(){ {Piccolo, 1}, {Std, 2}, {Grande, 3} };
+    public static Dictionary<belag, int> belagpay = new Dictionary<belag,int>(){ {cheese, 1}, {salami, 2} };
+
+    public static int preis(size g, List<belag> b) {
+        var result = sizepay[g];
+        foreach (var x in b) result += belagpay[x];
+        return result;
+    }
+}
